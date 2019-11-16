74fa56bf1862806670efeb62db0c4128a67467b1
Vivek Kumar UAE <Vivek.Kumar@XEBIAINDIA.LOCAL>
2019-11-16 23:36:56 +0530

    Adding learning for JavaScript

---
+++ index.js
@@ -0,0 +1,13 @@
+let person ={
+    name:'Vivek',
+    age:28
+};
+//console.log(person);
+
+let selectedColors = ['Green','Red','Blue'];
+//console.log(selectedColors);
+
+function greet(name) {
+    console.log('Hi ' + name);
+}
+greet('Vivek');
